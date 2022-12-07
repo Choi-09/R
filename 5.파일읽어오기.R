4db6cc2a56f7dd2a9aafa3d261673a1856fe8eb0
vanessajchoi <vanessajchoi@gmail.com>
2022-12-07 21:21:03 +0900

    txt, csv 파일 읽어오기

--- 5.파일읽어오기.R
+++ 5.파일읽어오기.R
@@ -1,4 +1,17 @@
 # 파일 읽어오기
-setWd("C:/06R/R프로그래밍")
-# apt <- read.table(file.choose(), header =TRUE)  # 해당 파일을 불러오고 첫 줄을 header로 쓰겠다.
-apt <- read.table("C:/06R/R프로그래밍/atp.txt", header = TRUE)
+# (1) 파일 선택. header, encoding 필수!
+data <- read.csv(file.choose(), header = T, fileEncoding = 'euc-kr')
+
+# head() : data의 처음 6번째 데이터
+head(data)
+
+# tail() : data의 끝 6번째 데이터
+tail(data)
+
+# str() : data의 구조조
+str(data)
+
+# max() : data의 열에서 가장 높은 값 
+max(data$가격)
+
+# min() : data의 열에서 가장 낮은 값 
